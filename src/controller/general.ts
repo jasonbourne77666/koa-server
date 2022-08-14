24ee1d39e14f0a9840d0959a7fdc2672e670a039
jasonbourne77666 <jasonbourne77666@gmail.com>
2022-08-14 21:54:22 +0800

    test

--- src/controller/general.ts
+++ src/controller/general.ts
@@ -1,5 +1,7 @@
 import { BaseContext, Context } from 'koa';
 import { description, request, summary, tagsAll, path, body } from 'koa-swagger-decorator';
+import { resJson } from '../utils/resJson';
+import { HttpStatus } from '../config/httpStatus';
 
 @tagsAll(['General'])
 export default class GeneralController {
@@ -14,7 +16,20 @@ export default class GeneralController {
   @summary('Welcome page')
   @description('A simple welcome message to verify the service is up and running.')
   public static async getBanner(ctx: BaseContext): Promise<void> {
-    ctx.body = 'getBanner';
+    ctx.body = resJson.success({
+      msg: '登录成功!',
+      code: HttpStatus.SUCCESS,
+      data: [
+        {
+          title: '网络头像测试1',
+          img: 'https://img1.baidu.com/it/u=4216761644,15569246&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=500',
+        },
+        {
+          title: '网络头像测试2',
+          img: 'https://img0.baidu.com/it/u=2106001853,3367829453&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
+        },
+      ],
+    });
   }
 
   @request('get', '/banner-detail/{id}')
